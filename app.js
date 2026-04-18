d2d2d79c416e5fdea4dfc5ef07392bc4f5fd2ea5
deepti1203 <deeptigore12@gmail.com>
2026-04-18 19:29:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -103,6 +103,11 @@ app.use((req, res, next) => {
     next(new ExpressError(404, "Page Not Found!"));
 });
 
+app.use((req, res, next) => {
+    res.locals.currUser = req.user;
+    next();
+});
+
 app.use((err, req, res, next) => {
     let {statusCode=500, message="something went wrong!"} = err;
     res.status(statusCode).render("error.ejs", { message });
